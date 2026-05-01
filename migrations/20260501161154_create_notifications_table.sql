855f6414e826dccd4fd80c172ec61a10da8b20c4
Rahman Umardi <rahmanumardi@gmail.com>
2026-05-01 16:15:22 +0700

    init migration table notification

---
+++ migrations/20260501161154_create_notifications_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS notifications (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    data TEXT,
+    read_at TIMESTAMP WITH TIME ZONE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
